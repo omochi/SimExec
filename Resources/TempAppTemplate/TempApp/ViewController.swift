bd4986a7e552c124db9b9991b79124e32b16378c
omochimetaru <omochi.metaru@gmail.com>
2019-05-10 19:34:51 +0900

    template

--- Resources/TempAppTemplate/TempApp/ViewController.swift
+++ Resources/TempAppTemplate/TempApp/ViewController.swift
@@ -3,8 +3,6 @@ import UIKit
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = UIColor.red
     }
 }
 
